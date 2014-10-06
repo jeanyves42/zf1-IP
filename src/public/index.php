db0bf50fd9a5c0c403c9ec9fc3c6d5e14189695b
jeanyves42 <jybruyas4265@orange.fr>
2014-10-06 12:57:27 +0200

    1er commit

---
+++ src/public/index.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(dirname(__DIR__)));
+define('SRC_PATH', ROOT_PATH . DS . 'src');
+define('PUBLIC_PATH', SRC_PATH . DS . 'public');
+define('APPLICATION_PATH', SRC_PATH . DS . 'application');
+define('CONFIG_PATH', APPLICATION_PATH . DS . 'configs');
+
+require_once ROOT_PATH . DS . 'vendor' . DS . 'autoload.php';
+
+$application = new Zend_Application(
+        // APPLICATION_ENV : variable que l'on définit dans .htaccess et que l'on récupère avec getenv
+        getenv('APPLICATION_ENV') ? : 'production', 
+        CONFIG_PATH . DS . 'application.ini'
+    );
+$application->bootstrap();
+$application->run();
